56fb1d258e6b8071e9d4ffbe93ce8eb895da6e7d
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2022-07-11 20:58:31 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createServer, Model } from 'miragejs'
+import { App } from './App'
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-07-11 12:00:00')
+        },
+
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Conta',
+          amount: 1100,
+          createdAt: new Date('2022-07-11 15:30:00')
+        },
+      ],
+    })
+  },
+
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
